4069c86402e8904f2850dc242e2039ecd917ba0f
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-01-29 10:21:16 -0300

    criacao das listas

--- tester.py
+++ tester.py
@@ -2,4 +2,8 @@ import gera_lista
 
 # print(gera_lista.gera_lista_sem_rep(100000))
 # print(gera_lista.random_unique_int_list(100000))
-print(gera_lista.random_unique_int_list_sample(1000000))
+# print(gera_lista.random_unique_int_list_sample(1000000))
+
+# print(gera_lista.cria_lista_inversamente_ordenada(1000000))
+# print(gera_lista.cria_lista_ordenada(1000000))
+# print(gera_lista.cria_lista_quase_ordenada(1000000))
