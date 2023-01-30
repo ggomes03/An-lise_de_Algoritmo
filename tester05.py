aacdba682a99be414412efd1de906ab85c65b021
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-01-29 21:08:58 -0300

    testes

---
+++ tester05.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+# sys.path.append(os.path.join(os.path.dirname("listas/"), "inversamente"))
+
+# sys.path.append(os.path.join(os.path.dirname("listas/"), "aleatorias"))
+sys.path.append(os.path.join(os.path.dirname("./"), "algoritmos"))
+sys.path.append(os.path.join(os.path.dirname("listas/"), "ordenadas"))
+# sys.path.append(os.path.join(os.path.dirname("listas/"), "quaseOrdenadas"))
+import Elista100k
+# import bubblesort
+# import insertionsort
+# import selectionsort
+# import quicksort
+# import heapsort
+import mergesort
+
+lista = Elista100k.lista
+
+# print(bubblesort.bubble_sort(lista))
+# print(insertionsort.insertion_sort(lista))
+# print(selectionsort.selection_sort(lista))
+# print(quicksort.quicksort(lista))
+# print(heapsort.heap_sort(lista))
+print(mergesort.merge_sort(lista))
